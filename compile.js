d29774c5d1a9f2a9eccd925c1e49054695e69c35
kyroslee <kyrosllk1996@gmail.com>
2021-03-19 03:37:36 +0800

    add compile script

---
+++ compile.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+
+console.log("Compiling...");
+fetch("https://closure-compiler.appspot.com/compile", {
+    method: "POST",
+    body: new URLSearchParams({
+        js_code: fs.readFileSync(path.join(__dirname, "main.js")),
+        compilation_level: "SIMPLE_OPTIMIZATIONS",
+        output_format: "json",
+        output_info: "compiled_code"
+    })
+})
+.then(res => res.json())
+.then(({ compiledCode }) => {
+    fs.writeFileSync(path.join(__dirname, "main.min.js"), compiledCode);
+    console.log("Finished");
+})
+.catch((e) => {
+    console.error(e);
+});
